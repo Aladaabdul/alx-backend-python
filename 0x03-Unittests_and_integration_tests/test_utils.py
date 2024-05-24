79b036b079411f5912a4f5d8ca1c0c7ffca4a77c
aladaabdul <aladarahman18@gmail.com>
2024-05-24 14:29:19 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,10 +4,11 @@
 """TestAccessNestedMap Module"""
 
 
-from utils import access_nested_map
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized, parameterized_class
-from typing import Mapping, Sequence, Any
+from utils import access_nested_map, get_json, memoize
+from typing import Mapping, Sequence, Any, Dict
 
 
 
@@ -47,3 +48,52 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson Class
+
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(
+            self,
+            url,
+            test_payload
+            ):
+        """test_get_json function
+
+        """
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        result = get_json(url)
+        self.assertEqual(result, test_payload)
+        mock.assert_called_once()
+        patcher.stop()
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize Class
+
+    """
+    def test_memoize(self):
+        """test_memoize function
+
+        """
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
